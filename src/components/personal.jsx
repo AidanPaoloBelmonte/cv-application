ac3f95f11333790b313cf806043a4c1351c8687c
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-10-15 15:12:12 +0800

    feat: Submit button is now disabled when form is invalid feat: Email and Telephone fields now have proper pattern matching

--- src/components/personal.jsx
+++ src/components/personal.jsx
@@ -1,50 +1,112 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 import "../styles/style.css";
 
-function FormEntry({ name, label, inputType = "field", halfCol = false }) {
+function FormEntry({
+  name,
+  label,
+  ref,
+  inputType = "field",
+  onChange,
+  onBlur,
+  halfCol = false,
+  asPlain = false,
+}) {
   const [value, setValue] = useState("");
 
-  const onChange = (e) => {
+  const onInputChange = (e) => {
+    onChange(e);
+
     setValue(e.target.value);
   };
 
   let className = "formEntry";
   if (halfCol) className += " halfCol";
 
+  const entry = !asPlain ? (
+    <input
+      type={inputType}
+      name={name}
+      onChange={onInputChange}
+      onBlur={onBlur}
+      ref={ref}
+      required
+    ></input>
+  ) : (
+    <p>{value}</p>
+  );
+
   return (
     <>
       <div className={className}>
         <label for={name}>
           {label}
-          {!value ? <span style={{ color: "red" }}>*</span> : null}
+          {!asPlain && !value ? <span style={{ color: "red" }}>*</span> : null}
         </label>
-        <input
-          type={inputType}
-          name={name}
-          onChange={onChange}
-          required
-        ></input>
+        {entry}
       </div>
     </>
   );
 }
 
 function PersonalSection() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, isValid },
+  } = useForm({ mode: "OnChange" });
   const [submitted, setSubmitStatus] = useState(false);
+
+  const handleSubmitStateChange = (e) => {
+    setSubmitStatus(true);
+
+    e.preventDefault();
+  };
 
   return (
     <div className="personalSection">
-      <form>
+      <form onSubmit={handleSubmit()}>
         <div className="formEntry">
           <h2>Personal Information</h2>
         </div>
-        <FormEntry name="firstName" label="First Name" halfCol={true} />
-        <FormEntry name="lastName" label="Last Name" halfCol={true} />
-        <FormEntry name="emailAdd" label="Email Address" inputType="email" />
-        <FormEntry name="phoneNum" label="Phone Number" inputType="tel" />
+        <FormEntry
+          {...register("firstName", { required: true })}
+          label="First Name"
+          halfCol={true}
+          asPlain={submitted}
+        />
+        <FormEntry
+          {...register("lastName", { required: true })}
+          label="Last Name"
+          halfCol={true}
+          asPlain={submitted}
+        />
+        <FormEntry
+          {...register("emailAdd", {
+            required: true,
+            pattern: /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm,
+          })}
+          label="Email Address"
+          inputType="email"
+          asPlain={submitted}
+        />
+        <FormEntry
+          {...register("phoneNum", {
+            required: true,
+            pattern:
+              /^\(?\+?([0-9]{1,3})?[- (]?\(?([0-9]{3})?[-. )]*?\(?([0-9]{3})[-. )]*?\(?([0-9]{4})\)?\s?$/gm,
+          })}
+          label="Phone Number"
+          inputType="tel"
+          asPlain={submitted}
+        />
         <div className="formFooter">
-          <button submit="button" onClick={() => setSubmitStatus(true)}>
+          <button
+            submit="button"
+            onClick={handleSubmitStateChange}
+            disabled={!isDirty || (!isValid && !submitted)}
+          >
             Submit
           </button>
         </div>
